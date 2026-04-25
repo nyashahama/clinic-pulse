1a016adf07e8093a63fc9aa82c3d3e1c4076f498
Nyasha Hama <nyashahama45@gmail.com>
2026-04-25 19:44:20 +0200

    feat: add use-scroll, use-scroll-progress, use-resize-observer hooks

---
+++ lib/hooks/use-resize-observer.ts
@@ -0,0 +1,22 @@
+import { RefObject, useEffect, useState } from "react";
+
+export function useResizeObserver(
+  elementRef: RefObject<Element | null>,
+): ResizeObserverEntry | undefined {
+  const [entry, setEntry] = useState<ResizeObserverEntry>();
+
+  const updateEntry = ([entry]: ResizeObserverEntry[]): void => {
+    setEntry(entry);
+  };
+
+  useEffect(() => {
+    const node = elementRef?.current;
+    if (!node) return;
+
+    const observer = new ResizeObserver(updateEntry);
+    observer.observe(node);
+    return () => observer.disconnect();
+  }, [elementRef]);
+
+  return entry;
+}
