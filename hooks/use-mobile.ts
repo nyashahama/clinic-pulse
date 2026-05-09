635ad293d82cf25226ff549880b117eda027c81a
Nyasha Hama <nyashahama45@gmail.com>
2026-05-09 17:32:19 +0200

    Fix mobile hook lint failure

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,19 +1,31 @@
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
+const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+
+function subscribeToMobileQuery(onStoreChange: () => void) {
+  if (typeof window === "undefined") {
+    return () => {}
+  }
+
+  const mediaQueryList = window.matchMedia(MOBILE_QUERY)
+  mediaQueryList.addEventListener("change", onStoreChange)
+
+  return () => mediaQueryList.removeEventListener("change", onStoreChange)
+}
+
+function getMobileSnapshot() {
+  return typeof window !== "undefined" && window.matchMedia(MOBILE_QUERY).matches
+}
+
+function getServerMobileSnapshot() {
+  return false
+}
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isMobile
+  return React.useSyncExternalStore(
+    subscribeToMobileQuery,
+    getMobileSnapshot,
+    getServerMobileSnapshot
+  )
 }
